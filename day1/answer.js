ca70f3e3d49c7894b1579e7e746f25e646bbfc46
m4tchka <91279022+m4tchka@users.noreply.github.com>
2022-12-01 14:34:28 +0000

    day1 comments

--- day1/answer.js
+++ day1/answer.js
@@ -4,16 +4,20 @@ let puzzleInput = readFileSync("./input.txt", "utf-8");
 
 function findMostCalories(data) {
     let arr = data.split("\r\n\r\n"); 
-    // Split array by each double line break (Windows)
-    // ["1000\r\n2000\r\n3000\r\n, 4000"]
-    console.log(arr)
+    // Split array by each double line break (Windows, so CRLF)
+    // console.log(arr) // ["1000\r\n2000\r\n3000", "4000", "5000\r\n6000", ...]
     for (let i = 0; i < arr.length; i++) {
+        // Loop through array
         arr[i] = arr[i]
             .split("\r\n")
+            // Split each element by its line breaks into several strings, 
             .map(Number)
+            // Turn each newly-split element into a number
             .reduce((acc, curr) => acc + curr);
+            // Sum all the elements in each sub-array of numbers and assign that sum as the new value of the array entry.
     }
     return Math.max(...arr);
+    // Return the largest entry in the array
 }
 console.log(findMostCalories(puzzleInput)); // 71502
 
@@ -26,7 +30,10 @@ function find3MostCalories(data) {
             .map(Number)
             .reduce((acc, curr) => acc + curr);
     }
+    // As above ...
     arr.sort((a, b) => a - b);
+    // Sort array by ascending value
     return arr[arr.length - 1] + arr[arr.length - 2] + arr[arr.length - 3];
+    // Return the sum of the largest (last) 3 entries
 }
 console.log(find3MostCalories(puzzleInput)); // 208191
