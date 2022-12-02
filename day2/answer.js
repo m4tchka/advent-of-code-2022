85991b2d3ccab072f6c0a5d25b26a72ce3df0954
m4tchka <91279022+m4tchka@users.noreply.github.com>
2022-12-02 15:31:54 +0000

    reorganise part 1 + comments

--- day2/answer.js
+++ day2/answer.js
@@ -5,36 +5,43 @@ let puzzleInput = readFileSync("./input.txt", "utf-8").split("\r\n");
 function calcMostScore(arr) {
     let score = 0;
     for (let i = 0; i < arr.length; i++) {
-        let roundScore = 0;
         arr[i] = arr[i].split(" ");
-        if (
-            (arr[i][0] === "A" && arr[i][1] === "Y") ||
-            (arr[i][0] === "B" && arr[i][1] === "Z") ||
-            (arr[i][0] === "C" && arr[i][1] === "X")
-        ) {
-            roundScore += 6;
-        } else if (
-            (arr[i][0] === "A" && arr[i][1] === "X") ||
-            (arr[i][0] === "B" && arr[i][1] === "Y") ||
-            (arr[i][0] === "C" && arr[i][1] === "Z")
-        ) {
-            roundScore += 3;
-        } else {
-            roundScore += 0;
-        }
-        switch (arr[i][1]) {
-            case "X":
-                roundScore++;
-                break;
-            case "Y":
-                roundScore += 2;
-                break;
-            case "Z":
-                roundScore += 3;
-                break;
-        }
+        let roundScore = 0;
+        roundScore += calcRoundOutcome(arr[i][0], arr[i][1]);
+        roundScore += calcPlayerMoveScore(arr[i][1]);
+        // For each round, get the score from the outcome of the round and the player's move. Then add the total score for the round to the total score
         score += roundScore;
     }
     return score;
+}
+function calcRoundOutcome(cpuMove, playerMove) {
+    if (
+        (cpuMove === "A" && playerMove === "Y") ||
+        (cpuMove === "B" && playerMove === "Z") ||
+        (cpuMove === "C" && playerMove === "X")
+    ) {
+        return 6;
+        // Win condition
+    } else if (
+        (cpuMove === "A" && playerMove === "X") ||
+        (cpuMove === "B" && playerMove === "Y") ||
+        (cpuMove === "C" && playerMove === "Z")
+    ) {
+        return 3;
+        // Draw condition
+    } else {
+        return 0;
+        // Loss condition
+    }
+}
+function calcPlayerMoveScore(playerMove) {
+    switch (playerMove) {
+        case "X":
+            return 1;
+        case "Y":
+            return 2;
+        case "Z":
+            return 3;
+    }
 }
 console.log(calcMostScore(puzzleInput));
