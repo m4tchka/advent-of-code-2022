ef421e75ec1443e4d2926690876ed0529f777e7e
m4tchka <91279022+m4tchka@users.noreply.github.com>
2022-12-04 16:57:08 +0000

    day 4 part 2

--- day4/answer.js
+++ day4/answer.js
@@ -18,7 +18,33 @@ function countSubsets(arr) {
 
     return subsets;
 }
-console.log(countSubsets(puzzleInput));
+// console.log(countSubsets(puzzleInput));
 
-//   ............21.....................71
-//   .....8..............40................
+// Part 2
+function countOverlaps(arr) {
+    let overlaps = 0;
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = arr[i].split(",");
+        let [elf1Start, elf1End] = arr[i][0].split("-").map(Number);
+        let [elf2Start, elf2End] = arr[i][1].split("-").map(Number);
+        if (
+            !(
+                (elf1Start < elf2Start && elf1End < elf2Start) ||
+                (elf1Start > elf2End && elf1End > elf2End)
+            )
+        ) {
+            overlaps++;
+        }
+    }
+    return overlaps;
+}
+console.log(countOverlaps(puzzleInput));
+
+// ..2...4......
+// .........6...8..
+
+// ..2......6...
+// .....4......8...
+
+// ... x.......y....
+// .a.b.....
