321b4334c4c997241f8d81ed154fe3810a81f851
m4tchka <91279022+m4tchka@users.noreply.github.com>
2022-12-05 23:38:17 +0000

    part1 parsing

--- day5/answer.js
+++ day5/answer.js
@@ -4,16 +4,19 @@ let input = readFileSync("./inputTest.txt", "utf-8").split("\r\n\r\n");
 // console.log(input)
 let crates = input[0].split("\r\n");
 let columns = crates.pop().split("   ")
-let numOfColumns=columns[columns.length - 1]
+let numOfColumns=Number(columns[columns.length - 1])
 console.log("crates",crates)
-for (let crateLine of crates) {
-    console.log("split iine:",crateLine.split(/(\.|s){4}/))
-    for (let char = 0; char < ((crateLine.length-(numOfColumns-1))/3); char++) {
-        console.log(crateLine.substring(char*4,(char*4)+3))
+let stack = new Array(numOfColumns).fill([])
+for (let crateRow of crates) {
+    // console.log("split iine:",crateRow.split(/(\.|s){4}/))
+    for (let char = 0; char < ((crateRow.length-(numOfColumns-1))/3); char++) {
+        let crate = crateRow.substring(char*4,(char*4)+3)
+        console.log(crate)
+        stack[char].push(crate[1])
     }
     // console.log(crateLine.length)
 }
 // crates
 
-
+console.log(stack)
 // 123 567 901
